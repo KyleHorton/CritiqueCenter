9fd5669b5cbcbdfe8b75e16a3a20387937327a5a
Mark Russo <markrusso@Markys-MacBook-Pro.local>
2018-04-16 17:55:07 -0400

    Splash Screen

--- app/src/main/java/kylehorton/ser210/quinnipiac/edu/critiquecenter/MainActivity.java
+++ app/src/main/java/kylehorton/ser210/quinnipiac/edu/critiquecenter/MainActivity.java
@@ -1,15 +1,25 @@
 package kylehorton.ser210.quinnipiac.edu.critiquecenter;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    //does this work??
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //splash screen shown
+        int secondsDelayed = 5;
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                startActivity(new Intent(MainActivity.this, HomeScreenActivity.class));
+                finish();
+            }
+        }, secondsDelayed * 1000);
     }
 }
+
