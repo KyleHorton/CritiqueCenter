c56bfafc888438ebfcc99a04c6600c5e1aeae584
Mark Russo <markrusso@Markys-MacBook-Pro.local>
2018-04-16 17:44:31 -0400

    test

--- app/src/main/java/kylehorton/ser210/quinnipiac/edu/critiquecenter/MainActivity.java
+++ app/src/main/java/kylehorton/ser210/quinnipiac/edu/critiquecenter/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //does this work??
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
